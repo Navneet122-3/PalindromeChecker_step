36f47b30f9c959c5b874eb33e71fd48fb57b809d
Navneet122-3 <as8888@srmist.edu.in>
2026-03-02 15:43:25 +0530

    Queue and Stack implementation

--- src/palindromeCheckerApp/UseCasePalindromeCheckerApp.java
+++ src/palindromeCheckerApp/UseCasePalindromeCheckerApp.java
@@ -1,33 +1,35 @@
 package palindromeCheckerApp;
 import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
+
 public class UseCasePalindromeCheckerApp {
 
     public static void main(String[] args) {
 
+        String input = "civic";
 
-        String input = "noon";
-
+        Queue<Character> queue = new LinkedList<>();
 
         Stack<Character> stack = new Stack<>();
 
-
         for (char c : input.toCharArray()) {
+            queue.add(c);
             stack.push(c);
         }
 
-
         boolean isPalindrome = true;
 
+        while (!queue.isEmpty()) {
 
-        for (char c : input.toCharArray()) {
+            char fromQueue = queue.remove();  // Dequeue
+            char fromStack = stack.pop();     // Pop
 
-
-            if (c != stack.pop()) {
+            if (fromQueue != fromStack) {
                 isPalindrome = false;
                 break;
             }
         }
-
 
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
