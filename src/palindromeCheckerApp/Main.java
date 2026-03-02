fa4315c450068c96c223df461bd1b722cee2437d
Navneet122-3 <as8888@srmist.edu.in>
2026-03-02 15:18:44 +0530

    2nd commit

--- src/palindromeCheckerApp/Main.java
+++ src/palindromeCheckerApp/Main.java
@@ -3,6 +3,11 @@ package palindromeCheckerApp;
 public class Main {
     static void main(String[] args) {
         System.out.println("Welcome to my Palindrome Checker App");
+
+
+
+            for (int i = 1; i <= 5; i++) IO.println("i = " + i);
+        }
     }
-}
+
 
